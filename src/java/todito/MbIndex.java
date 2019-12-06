751987b31b6054a9972d5cca5e1e1b0cf2c96e25
Gabriel Rodriguez <48400046+bonnesbaby14@users.noreply.github.com>
2019-12-05 19:10:51 -0600

    PINTOR LE VIO EL PITO A SYDNEY

--- src/java/todito/MbIndex.java
+++ src/java/todito/MbIndex.java
@@ -34,7 +34,7 @@ public class MbIndex implements Serializable {
     private Encuesta formSelected;
     private ArrayList<Pregunta> cmb2 = null;
     private ArrayList<Preguntaabcd> cmb3 = null;
-    private ArrayList<Pregunta> cmb4 = null;    
+    private ArrayList<Pregunta> cmb4 = null;
 
     public Encuesta getFormSelected() {
         return formSelected;
@@ -43,23 +43,21 @@ public class MbIndex implements Serializable {
     public void setFormSelected(Encuesta formSelected) {
         this.formSelected = formSelected;
     }
-    
-    
 
     public ArrayList<Encuesta> getCmb1() {
-        
-        cmb1=new ArrayList<Encuesta>();
+
+        cmb1 = new ArrayList<Encuesta>();
         EntityManagerFactory emf;
         EntityManager em;
         emf = Persistence.createEntityManagerFactory("CocoFormsPU");
         em = emf.createEntityManager();
         Query consulta = em.createNamedQuery("FindtoAllEncuestas");
-         List <Encuesta> tipo= consulta.getResultList(); 
-         System.out.println("holaaaa");
-          for(Encuesta encuesta : tipo){
-              cmb1.add(encuesta);
-          }
-         
+        List<Encuesta> tipo = consulta.getResultList();
+        System.out.println("holaaaa");
+        for (Encuesta encuesta : tipo) {
+            cmb1.add(encuesta);
+        }
+
         return cmb1;
     }
 
@@ -75,57 +73,67 @@ public class MbIndex implements Serializable {
         this.cmb1 = cmb1;
     }
 
+    public ArrayList<Preguntaabcd> getCmb3() {
+        return cmb3;
+    }
+
+    public void setCmb3(ArrayList<Preguntaabcd> cmb3) {
+        this.cmb3 = cmb3;
+    }
+
+    public ArrayList<Pregunta> getCmb4() {
+        return cmb4;
+    }
+
+    public void setCmb4(ArrayList<Pregunta> cmb4) {
+        this.cmb4 = cmb4;
+    }
+
     public MbIndex() {
 
     }
-    
-    public String toForm(Encuesta encuesta){
-        formSelected=encuesta;
-        cmb2=new ArrayList<Pregunta>();
-        cmb3=new ArrayList<Preguntaabcd>();
+
+    public String toForm(Encuesta encuesta) {
+        formSelected = encuesta;
+        cmb2 = new ArrayList<Pregunta>();
+        cmb3 = new ArrayList<Preguntaabcd>();
         EntityManagerFactory emf;
         EntityManager em;
         emf = Persistence.createEntityManagerFactory("CocoFormsPU");
         em = emf.createEntityManager();
-        
-        
-        
-        
+
         Query consulta = em.createNamedQuery("Preguntas");
         consulta.setParameter("encuesta", formSelected.getId());
-         List <Pregunta> tipo= consulta.getResultList(); 
-         
+        List<Pregunta> tipo = consulta.getResultList();
 
-         System.out.println("se va hacer el primer for");
-          for(Pregunta pregunta : tipo){
-              
-              System.out.println(pregunta.getTexto());
-              
-              cmb2.add(pregunta);
-              
-          }
-         System.out.println("se termino el primer for"); 
-          
-          
+        System.out.println("se va hacer el primer for");
+        for (Pregunta pregunta : tipo) {
+
+            System.out.println(pregunta.getTexto());
+
+            cmb2.add(pregunta);
+
+        }
+        System.out.println("se termino el primer for");
+
         Query consulta2 = em.createNamedQuery("Preguntasabcd");
         consulta2.setParameter("encuesta", formSelected.getId());
-         List <Preguntaabcd> tipo2= consulta2.getResultList(); 
+        List<Preguntaabcd> tipo2 = consulta2.getResultList();
+
+
+        System.out.println("se va hacer el segundo for");
+        for (Preguntaabcd preguntaabcd : tipo2) {
             
-         System.out.println(formSelected.getId());
-         System.out.println("se va hacer el segundo for");
-                   for(Preguntaabcd preguntaabcd : tipo2){
-              
-              System.out.println(preguntaabcd.getText());
-              
-              cmb3.add(preguntaabcd);
-              
-          }
-          
-          System.out.println("se va terminar el segundo for");
-        
-    
+            System.out.println(preguntaabcd.getText());
+
+            cmb3.add(preguntaabcd);
+
+        }
+
+        System.out.println("se va terminar el segundo for");
+
         return "form";
-       
+
     }
 
 }
